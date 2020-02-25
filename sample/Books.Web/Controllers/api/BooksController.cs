01bd6e7857e0711aa922cf81ee7df11aef46c907
halida <linjunhalida@gmail.com>
2020-02-25 10:31:35 +0800

    rename

--- sample/Books.Web/Controllers/api/BooksController.cs
+++ sample/Books.Web/Controllers/api/BooksController.cs
@@ -1,13 +1,13 @@
 using Books.Application;
 using Books.Domain;
-using GoodCrud.Web.Api.Controllers;
+using GoodCrud.Web;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GoodCrud.Web.Books
 {
     [Area("api")]
     [Route("api/[controller]")]
-    public class BooksController : CrudController<BookWebService, Book, IBooksUnitOfWork, BookDto, BookCreateUpdateDto, BookCreateUpdateDto, BookFilterDto>
+    public class BooksController : Api.Controllers.CrudController<BookWebService, Book, IBooksUnitOfWork, BookDto, BookCreateUpdateDto, BookCreateUpdateDto, BookFilterDto>
     {
         public BooksController(BookWebService service) : base(service) { }
     }
