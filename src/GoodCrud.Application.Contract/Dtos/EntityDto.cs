5e017d8bce1a51289e4f09e15c0378ccbae5ef8e
halida <linjunhalida@gmail.com>
2020-03-30 15:21:49 +0800

    enable null

--- src/GoodCrud.Application.Contract/Dtos/EntityDto.cs
+++ src/GoodCrud.Application.Contract/Dtos/EntityDto.cs
@@ -5,7 +5,7 @@ namespace GoodCrud.Application.Contract.Dtos
     public class EntityDto
     {
         public int Id { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
